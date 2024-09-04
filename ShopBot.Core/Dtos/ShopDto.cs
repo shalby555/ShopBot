2f1c1a0fea70bddf8a09989830c2383363a3a80f
Sergye_Naughty <shalby555@gmail.com>
2024-09-05 02:42:14 +0300

    Dots

---
+++ ShopBot.Core/Dtos/ShopDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopBot.Core.Dtos
+{
+	public class ShopDto
+	{
+		public int Id { get; set; }
+
+		public string? StoreAddress { get; set; }
+
+		public int? PickUpPointId { get; set; }
+
+		public List<UserDto> Users { get; set; }
+
+	}
+}
