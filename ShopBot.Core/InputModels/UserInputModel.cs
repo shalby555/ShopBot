eb596640421d23c536ac59c36644760c6b164fc5
Sergye_Naughty <shalby555@gmail.com>
2024-09-09 14:22:25 +0300

    UserInputModel

---
+++ ShopBot.Core/InputModels/UserInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopBot.Core.InputModels
+{
+	public class UserInputModel
+	{
+		public string UserName { get; set; }
+
+		public string Phone { get; set; }
+
+		public int RoleId { get; set; }
+
+		public int ShopId { get; set; }
+	}
+}
+
