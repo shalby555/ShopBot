d8f6556d008bc2493fa72fcf060c53ed070fefa6
Sergye_Naughty <shalby555@gmail.com>
2024-09-10 00:44:34 +0300

    query

--- ShopBot.DAL/Querys/UserQuerys.cs
+++ ShopBot.DAL/Querys/UserQuerys.cs
@@ -8,9 +8,10 @@ namespace ShopBot.DAL.Querys
 {
 	public class UserQuerys
 	{
-		public const string AddUserQuery = $"INSERT INTO \"User\"(\"Name\",\"Phone\",\"RoleId\",\"ShopId\") VALUES (@name, @phone, @roleid, @shopid);";
+		public const string AddUserQuery = $"INSERT INTO \"User\"(\"Name\",\"Phone\",\"RoleId\",\"ShopId\")" +
+			                               $" VALUES (@name, @phone, @roleid, @shopid);";
 
-		public const string UpdateUserQuary = $"update \"User\"set \"Name\" = @name where \"Id\"= @id";
+		public const string UpdateUserQuary = $"update \"User\"set \"Name\" = @name where \"Id\"= @id"; //а нахрена??? 
 
 		public const string GetAllUsersQuary = $"SELECT \"Id\" \"Name\" FROM \"User\";";
 
@@ -19,6 +20,8 @@ namespace ShopBot.DAL.Querys
 		public const string DeleteUserByIdQuery =  $"delete  FROM \"User\" where \"Id\"= @id";
 
 		public const string GetAllUsersWithOrdersQuery = $" SELECT FROM  \"User\" as U left join \"Order\" as O on O.\"ShopId\" = U.\"ShopId\";";
+
+		//public const string select U."Id",U."Name",O."Id",O."Date"from "User" as U left join "Order" as O on O."ShopId" = U."ShopId" 
 
 		//public const string GetAllUsersFullInfoQuery = $" 
 	}
