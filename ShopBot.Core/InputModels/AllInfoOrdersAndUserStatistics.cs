1e366d0a14b9c5953c8d6017e18872a7170e1d66
Sergye_Naughty <shalby555@gmail.com>
2024-09-10 14:32:40 +0300

    inputModels

---
+++ ShopBot.Core/InputModels/AllInfoOrdersAndUserStatistics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopBot.Core.InputModels
+{
+	 public class AllInfoOrdersAndUserStatistics
+	{
+		public int Id { get; set; }
+	}
+}
